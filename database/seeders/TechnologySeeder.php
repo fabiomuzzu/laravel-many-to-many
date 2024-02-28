39c454b46a248594816c4ab57d397249540c71ec
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-28 17:16:34 +0100

    added table and seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +17,24 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tecnologies = [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "SCSS",
+            "PHP",
+            "Laravel",
+            "Phyton",
+        ];
+
+        foreach ($tecnologies as $item) {
+            
+            $technology = New Technology();
+
+            $technology->name = $item;
+            $technology->slug = Str::slug($item, '-');
+            
+            $technology->save();
+        }
     }
 }
